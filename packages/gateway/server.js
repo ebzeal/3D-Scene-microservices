8ef7e2ec9647bbac6333d28d8da99d67f80a85c4
olu <ebenezerajayi@gmail.com>
2020-10-26 21:57:30 +0100

    :zap: add ReadMe doc  - add PM2 for process management

--- packages/gateway/server.js
+++ packages/gateway/server.js
@@ -4,7 +4,6 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import cors from 'cors';
 import fs from 'fs';
 import resolvers from './src/graphql/resolvers';
-// import routes from './api/routes';
 
 const app = express();
 app.use(cors());
@@ -18,7 +17,6 @@ const port = process.env.PORT || 5000;
 
 const typeDefs = gql(fs.readFileSync('./src/graphql/schema.graphql', { encoding: 'utf8' }));
 
-// const context = ({ req }) => ({ user: req.user && db.users.get(req.user.sub) });
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 apolloServer.applyMiddleware({ app, path: '/graphql' });
 
