817e2a1b3ceb0e02cdd1e37ce3c652084ece8190
olu <ebenezerajayi@gmail.com>
2020-10-27 11:25:13 +0100

    :sparkles: connect gateway to 2 services

--- packages/gateway/server.js
+++ packages/gateway/server.js
@@ -16,10 +16,12 @@ env.config();
 const port = process.env.PORT || 5000;
 
 const typeDefs = gql(fs.readFileSync('./src/graphql/schema.graphql', { encoding: 'utf8' }));
+const context = ({ req }) => ({ headers: req.headers });
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context });
 apolloServer.applyMiddleware({ app, path: '/graphql' });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server started on port ${port}`);
 });
